ef9a1138fa976f95a228a91df25e7c3a84857bc6
klastill <gyb0501@gmail.com>
2021-08-07 23:49:38 +0900

    websocket

--- src/server.js
+++ src/server.js
@@ -1,14 +1,26 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 const port =  3000;
-
 const handleListener = () => console.log(`Listening on http://localhost:${port}`);
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
 
-app.listen(port, handleListener);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+function handleConnection(socket) {
+  console.log(socket);
+} 
+
+wss.on("connection", handleConnection);
+
+server.listen(port, handleListener);
+
+// app.listen(port, handleListener);
